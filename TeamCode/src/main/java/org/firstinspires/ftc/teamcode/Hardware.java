a3a88e62834912e4001c3c14a8dfb2c22cb938a9
Spitz <cooltigers112@gmail.com>
2018-09-20 07:42:34 -0400

    Hardware class added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+/**
+ *This class is for setting up all the hardware components of the robot.
+ *This will have all the sensors, motors and servos declarations.
+ *It will also be used to initialize everything for autonomous
+ */
+public class Hardware {
+
+    public static final String MESSAGETAG = "5040MSG";
+
+    HardwareMap hwMap;
+
+    /**
+     * Constructor to set up the Hardwaremap
+     */
+    public Hardware() {
+        hwMap = null;
+    }
+
+    /**
+     *Method for initializing all the hardware components.
+     *Use at the beginning of code initialization
+     * @param ahwMap the hardware declaration being passed into this class
+     * @param auto boolean to run initializations for auto
+     */
+    public void init(HardwareMap ahwMap, boolean auto) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+    }
+}
