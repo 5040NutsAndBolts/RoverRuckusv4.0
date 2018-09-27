19eadfe134dd0e09fadb71cae1da0e89f4366cc6
MJ <mortikid007@gmail.com>
2018-09-26 21:00:47 -0400

    Flywheel file now has programming for paddle servo at the end of the extender (probably going to be moved to a different class later)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelTest.java
@@ -1,25 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Color;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="Flywheel Test", group="Linear Opmode")
 public class FlywheelTest extends LinearOpMode {
-    //Servo linActuator = null;
+    Servo paddle = null;
     DcMotor wheelSpinnerClkWise = null;
     DcMotor wheelSpinnerCoClWise = null;
     ColorSensor iBaller = null;
 
     // Green value on the color sensor for gold
     private final double GOLD_COLOR_VAL = 100;
-    // Speed when gold is detected
-    private final double GOLD_SPEED = 1;
-    // Speed when silver is detected
-    private final double SILV_SPEED = 0.8;
+    // Positions for the paddle servo
+    private final double GOLD_POS = 1;
+    private final double SILV_POS = 0;
 
     public void runOpMode(){
         //linActuator = hardwareMap.servo.get("armExtension");
@@ -27,37 +26,27 @@ public class FlywheelTest extends LinearOpMode {
         wheelSpinnerCoClWise = hardwareMap.dcMotor.get("flySpinA");
         wheelSpinnerClkWise = hardwareMap.dcMotor.get("flySpinB");
         iBaller = hardwareMap.get(ColorSensor.class,"colorSensor");
+        paddle = hardwareMap.servo.get("paddleServo");
 
         float hsvValues[] = {0F, 0F, 0F};
 
         waitForStart();
         while(opModeIsActive()) {
+            // Setup stuff
+            wheelSpinnerCoClWise.setPower(1);
+            wheelSpinnerClkWise.setPower(-1);
             Color.RGBToHSV((int) (iBaller.red() * 255),
                     (int) (iBaller.green() * 255),
                     (int) (iBaller.blue() * 255),
                     hsvValues);
-            /* Flywheel:
-             *   If color sensor shows
-             *     -white, actuator goes down and flywheels slow down
-             *     -yellow, actuator goes up and flywheels go quicker
-             * NO SERVO
-             * 2 MOTORS FOR TESTING, GOING IN OPPOSITE DIRECTIONS
-             */
-
-            if (iBaller.blue() <= GOLD_COLOR_VAL){
-                wheelSpinnerClkWise.setPower(GOLD_SPEED);
-                wheelSpinnerCoClWise.setPower(-GOLD_SPEED);
+            // Changes based on color sensor
+            if (iBaller.blue() <= GOLD_COLOR_VAL) {
                 telemetry.addLine("Gold!");
-                // Linear actuator goes down
+                paddle.setPosition(GOLD_POS);
             } else {
-                wheelSpinnerClkWise.setPower(SILV_SPEED);
-                wheelSpinnerCoClWise.setPower(-SILV_SPEED);
                 telemetry.addLine("Not gold!");
-                // Linear actuator goes up
+                paddle.setPosition(SILV_POS);
             }
-            telemetry.addLine("Red: "+iBaller.red());
-            telemetry.addLine("Blue: "+iBaller.blue());
-            telemetry.addLine("Green: "+iBaller.green());
             telemetry.update();
         }
     }
