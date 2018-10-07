d1df0c19e25081e7bb20653fa343ec3d62120714
Jackson <jed.chancey@gmail.com>
2018-10-07 18:03:45 -0400

    finished Setup for openCV

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -50,10 +50,12 @@ import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -118,14 +120,92 @@ import org.firstinspires.ftc.robotcore.internal.ui.UILocation;
 import org.firstinspires.ftc.robotcore.internal.webserver.RobotControllerWebInfo;
 import org.firstinspires.ftc.robotcore.internal.webserver.WebServer;
 import org.firstinspires.inspection.RcInspectionActivity;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import ftc.vision.FrameGrabber;
+
 @SuppressWarnings("WeakerAccess")
 public class FtcRobotControllerActivity extends Activity
   {
-  public static final String TAG = "RCActivity";
+    ////////////// START VISION PROCESSING CODE //////////////
+
+    // Loads camera view of OpenCV for us to use. This lets us see using OpenCV
+    private CameraBridgeViewBase cameraBridgeViewBase;
+
+    void myOnCreate(){
+      getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+      cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.show_camera_activity_java_surface_view);
+      new FrameGrabber(cameraBridgeViewBase);
+    }
+
+    //when the "Grab" button is pressed
+    public void frameButtonOnClick(View v){
+    }
+
+    void myOnPause(){
+      if (cameraBridgeViewBase != null) {
+        cameraBridgeViewBase.disableView();
+      }
+    }
+
+    void myOnResume(){
+      if (!OpenCVLoader.initDebug()) {
+        Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+      } else {
+        Log.d(TAG, "OpenCV library found inside package. Using it!");
+        mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+      }
+    }
+
+    public void myOnDestroy() {
+      if (cameraBridgeViewBase != null) {
+        cameraBridgeViewBase.disableView();
+      }
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+      @Override
+      public void onManagerConnected(int status) {
+        switch (status) {
+          case LoaderCallbackInterface.SUCCESS:
+            Log.i(TAG, "OpenCV Manager Connected");
+            //from now onwards, you can use OpenCV API
+//          Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
+            cameraBridgeViewBase.enableView();
+            break;
+          case LoaderCallbackInterface.INIT_FAILED:
+            Log.i(TAG, "Init Failed");
+            break;
+          case LoaderCallbackInterface.INSTALL_CANCELED:
+            Log.i(TAG, "Install Cancelled");
+            break;
+          case LoaderCallbackInterface.INCOMPATIBLE_MANAGER_VERSION:
+            Log.i(TAG, "Incompatible Version");
+            break;
+          case LoaderCallbackInterface.MARKET_ERROR:
+            Log.i(TAG, "Market Error");
+            break;
+          default:
+            Log.i(TAG, "OpenCV Manager Install");
+            super.onManagerConnected(status);
+            break;
+        }
+      }
+    };
+
+    ////////////// END VISION PROCESSING CODE //////////////
+
+
+    public static final String TAG = "RCActivity";
   public String getTag() { return TAG; }
 
   private static final int REQUEST_CONFIG_WIFI_CHANNEL = 1;
