7ccc8feb3e55bbc3fdc20030611364b85436117c
Spitz <cooltigers112@gmail.com>
2018-10-14 12:33:11 -0400

    Created Branch for Auto Positioning. Made a class for testing out how it works for now. Will make other things later. Issue #9

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPosTesting.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class AutoPosTesting extends LinearOpMode {
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
